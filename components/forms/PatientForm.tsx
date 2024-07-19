d1383d5b4657e7fa5141a5e041e52a656ceaaf06
sujal patel <worldonwheels701@gmail.com>
2024-07-19 08:16:34 -0500

    new custom form fields

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,5 +1,5 @@
 "use client"
- 
+
 import { Button } from "@/components/ui/button"
 import {
   Form,
@@ -14,6 +14,19 @@ import { Input } from "@/components/ui/input"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
+import CustomFormField from "../CustomFormField"
+import SubmitButton from "../SubmitButton"
+import { useState } from "react"
+
+export enum FormFieldType {
+  INPUT='input',
+  TEXTAREA='textarea',
+  CHECKBOX='checkbox',
+  PHONE_INPUT='phoneInput',
+  DATE_PICKER='datePicker',
+  SELECT='select',
+  SKELETON='skeleton',
+}
  
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -23,6 +36,9 @@ const formSchema = z.object({
  
 const PatientForm=()=> {
   // 1. Define your form.
+  const [isLoading, setIsLoading] = useState(false);
+
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -38,24 +54,41 @@ const PatientForm=()=> {
   }
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 flex-1">
+      <section className="mb-12 space-y-4">
+          <h1 className="header">Hi there 👋</h1>
+          <p className="text-dark-700">Get started with appointments.</p>
+        </section>
+        
+        <CustomFormField 
+        fieldType={FormFieldType.INPUT}
+        control={form.control}
+        name="name"
+        label="Full name"
+        placeholder="Sujal Patel"
+        iconSrc="/assets/icons/user.svg"
+        iconAlt="User"
         />
-        <Button type="submit">Submit</Button>
+
+        <CustomFormField 
+        fieldType={FormFieldType.INPUT}
+        control={form.control}
+        name="email"
+        label="Email"
+        placeholder="SP701@gmail.com"
+        iconSrc="/assets/icons/email.svg"
+        iconAlt="Email"
+        />
+
+        <CustomFormField
+          fieldType={FormFieldType.PHONE_INPUT}
+          control={form.control}
+          name="phone"
+          label="Phone number"
+          placeholder="9898112277"
+        />
+
+        <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
       </form>
     </Form>
   )
